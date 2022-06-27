4b93a5e2655a2b0f911dc70804cffd51e1fa8682
imroshin <imroshin@gmail.com>
2022-06-27 20:41:52 +0530

    config

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+
+const { API_URL, PRIVATE_KEY, tETHERSCAN_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.9",
+   defaultNetwork: "goerli",
+   networks: {
+      hardhat: {},
+      goerli: {
+         url: API_URL,
+         accounts: [PRIVATE_KEY]
+      }
+   },
+   etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: tETHERSCAN_KEY
+    },
+}
